0240571c2ad2d35e0ee46495e603423eb3939a5b
Rafael Chagas Sanches <rafa_0033@hotmail.com>
2023-09-28 22:01:58 -0300

    Add files via upload

---
+++ oficina/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$nome_oficina = "Oficina Freitas";
+$url = "http://localhost/oficina/";
+$endereco_oficina = "Rua Ali Perto, 984, São Paulo";
+$telefone_oficina = "(11) 12345-6789";
+$email_adm = "administrador@mail.com.br";
+
+//VARIAVEIS DO BANCO DE DADOS
+$servidor = 'localhost';
+$usuario = 'root';
+$senha = '';
+$banco = 'oficina';
+
+
+?>
--- oficina/painel-adm/index.php
+++ oficina/painel-adm/index.php
@@ -42,8 +42,9 @@
         <script src="../vendor/jquery/jquery.min.js"></script>
         <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
         
-         <link rel="shortcut icon" href="../../img/favicon0.ico" type="image/x-icon">
-    <link rel="icon" href="../../img/favicon0.ico" type="image/x-icon">
+        <!-- Logo da parte de cima do link -->
+        <link rel="shortcut icon" href="../img/logo-favicon.ico" type="image/x-icon">
+        <link rel="icon" href="../img/logo-favicon.ico" type="image/x-icon">
 
     </head>
 
